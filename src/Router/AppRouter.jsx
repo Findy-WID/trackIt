6fafadcbef897ab4ae6c8061ed1abafd8c84f1cb
Chisquare7 <chijiokechibuike.f@gmail.com>
2025-04-05 17:28:03 +0100

    update: fix collapsible sidebar blocking the main content when it expands or collapse

--- src/Router/AppRouter.jsx
+++ src/Router/AppRouter.jsx
@@ -1,45 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
-import  Landing  from "../Pages/Landing";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+import Landing from "../Pages/Landing";
 import { Home } from "../Pages/Home";
-import ExpenseAnalytics from '../Pages/ExpenseAnalytics';
-import ExpenseTablePage from '../Pages/ExpenseTablePage';
-import  Daily from "../Pages/Daily";
+import ExpenseAnalytics from "../Pages/ExpenseAnalytics";
+import ExpenseTablePage from "../Pages/ExpenseTablePage";
+import Daily from "../Pages/Daily";
 import Monthly from "../Pages/Monthly";
 import { Goals } from "../Pages/Goals";
 import { Sidebar } from "../Components/Sidebar";
 import { Link } from "react-router-dom";
-import SignUp from '../Pages/SignUp';
-import Login from '../Pages/Login';
-
+import SignUp from "../Pages/SignUp";
+import Login from "../Pages/Login";
 
 function AppRouter() {
-
   const location = useLocation();
-  const ShowSidebar = location.pathname !== "/"
+  const ShowSidebar = location.pathname !== "/";
 
   return (
-      <div className="App">
-          {ShowSidebar &&
-            <Sidebar />
-          }
-        <div className="content">
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/analysis" element={<ExpenseAnalytics />} />
-                <Route path="/goals" element={<Goals />} />
-                <Route path="/expense-table" element={<Navigate to="/expense-table/all" replace />} />
-                <Route path="/expense-table/all" element={<ExpenseTablePage />} />
-                <Route path="/expense-table/daily" element={<Daily />} />
-                <Route path="/expense-table/monthly" element={<Monthly />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<h2>404 - Page Not Found</h2>} />
-            </Routes>
-        </div>
+    <div className="App">
+      {ShowSidebar && <Sidebar />}
+      <div className={`contentWrapper ${ShowSidebar ? "withSidebar" : ""}`}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/analysis" element={<ExpenseAnalytics />} />
+          <Route path="/goals" element={<Goals />} />
+          <Route
+            path="/expense-table"
+            element={<Navigate to="/expense-table/all" replace />}
+          />
+          <Route path="/expense-table/all" element={<ExpenseTablePage />} />
+          <Route path="/expense-table/daily" element={<Daily />} />
+          <Route path="/expense-table/monthly" element={<Monthly />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<h2>404 - Page Not Found</h2>} />
+        </Routes>
       </div>
-  )
+    </div>
+  );
 }
 
 export default AppRouter;
