acdfe93305de5317a127cafaa6b2fdd0a4c3f67a
Findy <favouritefindy@gmail.com>
2025-04-07 04:39:07 +0100

    Saved changes to Expenses.jsx before merge

--- src/Pages/Expenses.jsx
+++ src/Pages/Expenses.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ExpenseForm from "../Components/ExpenseForm";
 import ExpenseOverview from "../Components/ExpenseOverview";
+import ExpenseList from '../Components/ExpenseList';
 // import "../Styles/Expenses.css";
 
 const Expenses = () => {
     return (
+      <>
+        <div>
+          <h2>Expenses</h2>
+          <span>Track and manage your expenses</span>
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-  <ExpenseForm />
-  <ExpenseOverview />
-</div>
+          <ExpenseForm />
+          <ExpenseOverview />
+        </div>
+        <div>
+          <ExpenseList />
+        </div>
+      </>
+    
 );
 };
 
